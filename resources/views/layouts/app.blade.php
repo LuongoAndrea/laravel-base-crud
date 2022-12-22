1e4302231763620a1677428d4059da53c6fa1a5e
AlessioBonadonna <alessiobonadonna23@gmail.com>
2022-12-22 17:10:05 +0100

    pietro ha fatto i bottoni
    Co-authored-by: Pietro Brighella <pietrobrighella@users.noreply.github.com>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,12 @@
     <title>Laravel</title>
 
     <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro:ital,wght@0,300;0,600;0,700;0,900;1,300;1,600&display=swap"
+        rel="stylesheet">
+    <script src="https://kit.fontawesome.com/49b8372efe.js" crossorigin="anonymous"></script>
     <!-- Styles -->
     @vite('resources/js/app.js')
 
--- resources/views/presents/index.blade.php
+++ resources/views/presents/index.blade.php
@@ -5,29 +5,56 @@
         <div class="row">
             @foreach ($presents as $present)
                 <div class="col-12 col-sm-6 col-md-4 mb-4">
-                    <div class="card">
-                        <img src="{{ Vite::asset('/resources/img/regalino.png') }}" class="card-img-top"
-                            alt="{{ $present->name }}">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $present->name }} {{ $present->cognome }}</h5>
-                            <p class="card-text">
-                                {{ $present->desc_regalo }}
-                                <br>
-                                {{ $present->citta }}
-                                <br>
-                                {{ $present->indirizzo }}
-                                <br>
-                                {{ $present->n_regalo }}
-                                <br>
-                                {{ $present->code_spedizione }}
-                                <br>
-                                {{ $present->camino }}
-                                <br>
-                                {{ $present->status }}
-                                <br>
+                    <div class="card-body">
+                        <img class="my-gif" src="{{ Vite::asset('/resources/img/icons8-tempesta-di-neve.gif') }}"
+                            alt="">
+                        <div class="text-center">
+                            <img src="{{ Vite::asset('/resources/img/regalino.png') }}"
+                                class="img-1  d-flex justify-content-center align-items-center" alt="{{ $present->name }}">
+                        </div>
+
+                        <hr>
+                        <h1 class="mb-4 card-title">{{ $present->name }} {{ $present->cognome }}</h1>
+                        <hr>
+                        <h4 class="mb-2">citta : {{ $present->citta }} </h4>
+                        <h4 class="mb-2">indirizzo : {{ $present->indirizzo }} </h4>
+                        <h4 class="mb-2">numeri regali : {{ $present->n_regalo }} </h4>
+                        <h4 class="mb-2">codice spedizione : {{ $present->code_spedizione }} </h4>
+                        <h4 class="mb-2">camino : {{ $present->camino }} </h4>
+                        <h4 class="mb-2">status : {{ $present->status }} </h4>
+                        <h4>regalo : {{ $present->desc_regalo }}</h4>
+
+                        <div id="" class=" my-2 align-items-center position-relative">
+
+                            <div class="d-flex justify-content-around">
+                                <a href="{{ route('presents.show', $present->id) }}" class="">
+                                    <button class="tuono_button my-3 tuono_btn_r"><i
+                                            class="fa-solid fa-circle-info"></i></button>
+                                </a>
+                                <a href="{{ route('presents.edit', $present->id) }}" class="">
+                                    <button class="tuono_button my-3 tuono_btn_r"><i
+                                            class="fa-solid fa-wand-magic-sparkles"></i></button>
+                                </a>
+                                <form action="{{ route('presents.destroy', $present->id) }}" class="" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="tuono_button my-3 tuono_btn_r"><i
+                                            class="ms-1 fa-solid fa-trash"></i></button>
+                                </form>
+
+                            </div>
+
+                        </div>
+                    </div>
 
 
-                            </p>
+
+
+
+
+
+
+                    {{--
                             <a href="{{ route('presents.show', $present->id) }}" class="btn btn-primary">Scopri!</a>
                             <a href="{{ route('presents.edit', $present->id) }}" class="btn btn-primary ms-3">Modifica</a>
                             <form action="{{ route('presents.destroy', $present->id) }}" method="POST">
@@ -37,7 +64,7 @@
                             </form>
 
                         </div>
-                    </div>
+                    </div> --}}
                 </div>
             @endforeach
 
